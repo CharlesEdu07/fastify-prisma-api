7424bb08d967c57b398175f1a1c264b330d674c6
Charles Eduardo <charleseduardofaria@gmail.com>
2024-03-30 17:34:27 -0300

    Adicionando servidor da aplicação

---
+++ src/app.ts
@@ -0,0 +1,50 @@
+import Fastify, { FastifyReply, FastifyRequest } from "fastify";
+
+import fjwt from "@fastify/jwt";
+
+import userRoutes from "./modules/user/user.route";
+
+import { userSchemas } from "./modules/user/user.schema";
+
+const server = Fastify();
+
+server.register(fjwt, {
+    secret: "supersecret",
+});
+
+server.decorate("authenticate", async (request: FastifyRequest, reply: FastifyReply) => {
+    try {
+        await request.jwtVerify();
+    } catch (err) {
+        return reply.send(err);
+    }
+});
+
+server.get("/healthcheck", async function () {
+    return {
+        status: "OK",
+    };
+});
+
+async function main() {
+    for (const schema of userSchemas) {
+        server.addSchema(schema);
+    }
+
+    server.register(userRoutes, { prefix: "api/users" });
+
+    try {
+        await server.listen({
+            host: "0.0.0.0",
+            port: 3000,
+        });
+
+        console.log(`Server running at http://localhost:3000/`);
+    } catch (err) {
+        server.log.error(err);
+
+        process.exit(1);
+    }
+}
+
+main();
