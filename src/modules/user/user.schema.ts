27f138a6986412ec942d40c71fc0cb023aa387ea
Charles Eduardo <charleseduardofaria@gmail.com>
2024-03-30 17:35:09 -0300

    Definindo userSchemas

---
+++ src/modules/user/user.schema.ts
@@ -0,0 +1,45 @@
+import { z } from 'zod';
+import { buildJsonSchemas } from 'fastify-zod';
+
+const userCore = {
+    name: z.string().min(3).max(255),
+    email: z.string({
+        required_error: 'Email is required',
+        invalid_type_error: 'Email must be a string',
+    }).email(),
+}
+
+const createUserSchema = z.object({
+    ...userCore,
+    password: z.string({
+        required_error: 'Password is required',
+        invalid_type_error: 'Password must be a string',
+    }).min(8).max(255),
+});
+
+const createUserResponseSchema = z.object({
+    id: z.number(),
+    ...userCore,
+});
+
+const loginSchema = z.object({
+    email: z.string({
+        required_error: 'Email is required',
+        invalid_type_error: 'Email must be a string',
+    }).email(),
+    password: z.string().min(8).max(255),
+});
+
+const loginResponseSchema = z.object({
+    accessToken: z.string(),
+});
+
+export type CreateUserInput = z.infer<typeof createUserSchema>;
+export type LoginInput = z.infer<typeof loginSchema>;
+
+export const { schemas: userSchemas, $ref } = buildJsonSchemas({
+    createUserSchema,
+    createUserResponseSchema,
+    loginSchema,
+    loginResponseSchema,
+});
